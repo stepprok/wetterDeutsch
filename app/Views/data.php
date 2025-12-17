39494da0185d215690e7edd6b7918a31f261db70
steprooauh <prokop_stepan@oauh.cz>
2025-12-17 08:19:11 +0100

    last

--- app/Views/data.php
+++ app/Views/data.php
@@ -1,6 +1,6 @@
 <?= $this->extend('layout/template'); ?>
 <?= $this->section('content'); ?>
-
+<br>
 <div class="d-flex justify-content-end mb-4">
     <a class="btn btn-outline-secondary" href="<?= base_url('stanice/' . $stanice->bundesland); ?>">
         <i class="fas fa-arrow-left me-2"></i> Zpět na seznam stanic
